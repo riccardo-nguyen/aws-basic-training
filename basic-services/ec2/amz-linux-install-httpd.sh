bcd512f342496c20aa9826c1d73875955597cbc8
Linh Nguyễn <riccardo.nguyen.1992@gmail.com>
2022-10-02 19:00:13 +0700

    modify\ user data install httpd

--- basic-services/ec2/amz-linux-install-httpd.sh
+++ basic-services/ec2/amz-linux-install-httpd.sh
@@ -8,7 +8,7 @@ cd /var/www/html
 echo "<html>" > index.html
 
 echo "<h1>Welcome to MyWebsite</h1>" >> index.html
-echo "<h4>You are running instance from this IP (This is for testing purpose only, you should not public this to user):</h4>"
+echo "<h3>You are running instance from this IP:</h3>"
 
 echo "<br>Private IP: " >> index.html
 curl http://169.254.169.254/latest/meta-data/local-ipv4 >> index.html
